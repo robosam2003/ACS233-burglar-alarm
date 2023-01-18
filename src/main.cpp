419096e9255308320554abd26cc2954c9b9c1c26
robos <robosam2003@gmail.com>
2023-01-18 10:57:40 +0000

    fixed door re-lock when re-close, and add beeping

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,11 @@
 uint64_t last_mag_switch_interrupt_time = 0; // in ms
 uint64_t last_key_sensor_interrupt_time = 0; // in ms
 
-uint64_t security_timer = 0; // in ms
+uint64_t last_door_open_time = -15000; // in ms
+uint64_t last_door_close_time = -15000; // in ms
 
+
+uint64_t security_timer = 0; // in ms
 
 /// Constants and enums
 // Enums to make the code more readable
@@ -50,9 +53,17 @@ enum LOCK_STATES {
     UNLOCKED = 1
 };
 
+enum PIR_STATES {
+    NO_PRESENCE = 0,
+    PRESENCE = 1
+};
+
 // States
+SYSTEM_MODES system_mode = ARMED; // initialised to DISARMED // TODO
+
 volatile LOCK_STATES lock_state = LOCK_STATES::LOCKED;
 volatile DOOR_STATES door_state = DOOR_STATES::CLOSED;
+volatile PIR_STATES pir_state = PIR_STATES::NO_PRESENCE;
 
 
 /// ISRs
@@ -63,6 +74,15 @@ void magSwitchISR() {
     if (millis() - last_mag_switch_interrupt_time > 100) {
         int state = digitalRead(MAG_SWITCH_PIN);
         door_state = (DOOR_STATES) state;
+
+        if (state == DOOR_STATES::CLOSED) {
+            last_door_close_time = millis();
+        } else {
+            last_door_open_time = millis();
+        }
+
+        Serial.print("Door state changed to: ");
+        Serial.println(state);
     }
     // update the last interrupt time
     last_mag_switch_interrupt_time = millis();
@@ -72,12 +92,15 @@ void keySensorISR() {
     if (millis() - last_key_sensor_interrupt_time > 100) { // debounce the interrupt using millis()
         security_timer = millis();
         lock_state = LOCK_STATES::UNLOCKED;
+        Serial.println("Lock state changed to: UNLOCKED");
     }
     last_key_sensor_interrupt_time = millis();
 }
 
 void PIRSensorISR() {
-    Serial.println("PIR sensor triggered! ");
+    // The PIR does not need debouncing, it goes High for 8 seconds when motion is detected
+    pir_state = PIR_STATES::PRESENCE;
+    Serial.println("PIR state changed to: PRESENCE");
 }
 
 /// Functions
@@ -93,7 +116,7 @@ void setup() {
      * Internal pull-up resistor means that when door is closed, the pin will read HIGH, and when door is open, the pin will read LOW.
      */
     pinMode(MAG_SWITCH_PIN, INPUT_PULLUP); // REED
-    attachInterrupt(digitalPinToInterrupt(MAG_SWITCH_PIN), magSwitchISR, RISING); // TODO consider change to RISING (door opening??? TODO )
+    attachInterrupt(digitalPinToInterrupt(MAG_SWITCH_PIN), magSwitchISR, CHANGE);
 
     /* Key sensor setup - We are using a push-button to simulate a key sensor.
      */
@@ -113,40 +136,50 @@ void setup() {
 
 void loop() {
 
-    int enter = 0; // temp variable that confirms user entry
-
+    int entered = 0; // checks for user entry
+    // authorized entry
     if (lock_state == LOCK_STATES::UNLOCKED) {
-        // Unlock door
-        digitalWrite(SOLENOID_PIN, HIGH);
-
-        // starts  warning buzzer
+        Serial.println("UNLOCKING DOOR");
+        digitalWrite(SOLENOID_PIN, HIGH); // Unlock door
         while (millis() - security_timer < 30000) {
-            if ((door_state = DOOR_STATES::CLOSED)) {
+            if (door_state == DOOR_STATES::CLOSED) {
                 if (millis() - security_timer > 10000) {
+                    // Door will re-lock after 10 seconds if not opened
+                    // (i.e. unlocked but not entered)
+                    Serial.println("LOCKING DOOR");
+                    lock_state = LOCK_STATES::LOCKED;
+                    digitalWrite(SOLENOID_PIN, LOW);
+                    break;
+                }
+                if (millis() - last_door_open_time < 10000) {
+                    // Door will re-lock after it has been opened in the past 10 seconds
+                    // (i.e. opened then closed)
+                    Serial.println("LOCKING DOOR");
                     lock_state = LOCK_STATES::LOCKED;
                     digitalWrite(SOLENOID_PIN, LOW);
                     break;
                 }
             }
-            if ((door_state = DOOR_STATES::OPEN)){
-                tone(BUZZER_PIN, 1000);
-                delay(100);
-                noTone(BUZZER_PIN);
+
+            // starts warning buzzer if door is open
+            if (door_state == DOOR_STATES::OPEN) {
+                switch (system_mode) {
+                    case (SYSTEM_MODES::ARMED):
+                        intermittentBeep(1000,50,500);
+                        break;
+                    case (SYSTEM_MODES::AT_HOME):
+                        intermittentBeep(1000,50,500);
+                        break;
+                    case (SYSTEM_MODES::DISARMED):
+                        break;
+
+                    default:
+                        break;
+                }
             }
-
-
-
-
-            delay(1000);
-            if (millis() - security_timer > 10000) {
-                lock_state = LOCK_STATES::LOCKED;
-                digitalWrite(SOLENOID_PIN, LOW);
-            }
-
+            delay(10);
         }
     }
-
-
 
 }
 
