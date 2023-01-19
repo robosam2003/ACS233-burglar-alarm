1b060a036cf5b32cdeb22830c7a80cb71969fd20
robos <robosam2003@gmail.com>
2023-01-19 11:25:38 +0000

    Simplified things and moved things to functions and finished unauthorisedEntry()

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,15 @@
 #define SOLENOID_PIN 12 // may be changed -
 
 #define BUZZER_PIN 7 //
-#define PIR_PIN 6
+#define PIR_PIN 18
 
-#define ALARM_FREQ 1000
-#define ALARM_TIMEOUT 10000 // 15*60*1000 // Should be 15 minutes in the end // TODO
-
-
-
-/// Global Variables
-int64_t last_mag_switch_interrupt_time = 0; // in ms - used for debouncing
-int64_t last_key_sensor_interrupt_time = 0; // in ms - used for debouncing
+#define ALARM_FREQ 500
+#define INTERMITTENT_TIMEOUT 15000 // 30 seconds
+#define ALARM_TIMEOUT (0.5*60*1000) // Should be 15 minutes in the end // TODO
 
 
-int64_t last_door_open_time = -100000; // in ms
-int64_t last_door_close_time = -100000; // in ms
-int64_t last_alarm_on_time = 0;
+
+
 
 
 // Macros
@@ -45,8 +39,6 @@ int64_t last_alarm_on_time = 0;
 
 
 
-
-int64_t security_timer = 0; // in ms
 
 /// Constants and enums
 // Enums to make the code more readable
@@ -87,6 +79,21 @@ SYSTEM_MODES system_mode = SYSTEM_MODES::AT_HOME; // initialised to DISARMED // 
 volatile LOCK_STATES lock_state = LOCK_STATES::LOCKED;
 volatile DOOR_STATES door_state = DOOR_STATES::CLOSED;
 volatile PIR_STATES pir_state = PIR_STATES::NO_PRESENCE;
+
+AUTHORISATION_STATES authorization = AUTHORISATION_STATES::UNAUTHORISED; //id password authorization variable
+
+
+/// Global Variables
+int64_t last_mag_switch_interrupt_time = 0; // in ms - used for debouncing
+int64_t last_key_sensor_interrupt_time = 0; // in ms - used for debouncing
+
+// Timers
+int64_t security_timer = 0; // in ms
+int64_t last_door_open_time = -100000; // in ms
+int64_t last_door_close_time = -100000; // in ms
+int64_t last_alarm_on_time = 0;
+int64_t last_mode_change_time = 0;
+
 
 
 /// ISRs
@@ -133,14 +140,14 @@ void PIRSensorISR() {
 
 /// Functions
 
-void beep(int freq, int onDel, int offDel){
+void beep(int freq, int onDel, int offDel) {
     tone(BUZZER_PIN, freq);
     delay(onDel);
     noTone(BUZZER_PIN);
     delay(offDel);
 }
 
-void alarmOn(){
+void alarmOn() {
     tone(BUZZER_PIN, ALARM_FREQ);
     last_alarm_on_time = millis();
 }
@@ -162,9 +169,37 @@ void unauthorizedEntry(UNAUTHORISED_ENTRY_METHODS method) {
                     break;
             }
 
-            
+            // Waiting for verified pin or 15 minutes elapsed
+            while ((authorization == AUTHORISATION_STATES::UNAUTHORISED) && (timeSince_ms(last_alarm_on_time) < ALARM_TIMEOUT)) {
+                // Check for Valid password // TODO
+                int a = timeSince_ms(last_alarm_on_time);
+
+            }
+            alarmOff(); // alarm switching off
+
             break;
         case PIR_TRIGGERED:
+            if ((timeSince_ms(security_timer) < INTERMITTENT_TIMEOUT) or (timeSince_ms(last_mode_change_time) < INTERMITTENT_TIMEOUT)) {
+                break;
+            } // if the security timer is less than 30 seconds, the alarm will not go off
+            else {
+                switch (system_mode) {
+                    case (SYSTEM_MODES::ARMED):   // when cases don't have a break, they fall through to the next case (i.e. this means OR)
+                        alarmOn(); // alarm switching on
+                        break;
+                    case (SYSTEM_MODES::AT_HOME):
+                    case (SYSTEM_MODES::DISARMED):
+                    default:
+                        break;
+                }
+
+
+                // Waiting for verified pin or 15 minutes elapsed
+                while (authorization == AUTHORISATION_STATES::UNAUTHORISED && timeSince_ms(last_alarm_on_time) < ALARM_TIMEOUT) {
+                    // Check for Valid password // TODO
+                }
+                alarmOff(); // alarm switching off
+            }
             break;
     }
 }
@@ -177,7 +212,7 @@ void setup() {
     /* Magnetic switch setup - Resistance of sensor goes very high when door is OPEN, and very low when door is CLOSED.
      * Internal pull-up resistor means that when door is closed, the pin will read HIGH, and when door is open, the pin will read LOW.
      */
-    pinMode(MAG_SWITCH_PIN, INPUT_PULLUP); // REED
+    pinMode(MAG_SWITCH_PIN, INPUT_PULLUP); //
     attachInterrupt(digitalPinToInterrupt(MAG_SWITCH_PIN), magSwitchISR, CHANGE);
 
     /* Key sensor setup - We are using a push-button to simulate a key sensor.
@@ -197,68 +232,19 @@ void setup() {
 }
 
 void loop() {
-
-    AUTHORISATION_STATES authorization = AUTHORISATION_STATES::UNAUTHORISED; //id password authorization variable
-
-    // authorized entry
+    // Check for door state change
     if (lock_state == LOCK_STATES::UNLOCKED) {
-//        Serial.println("UNLOCKING DOOR"); - Now happening in the ISR
-//        digitalWrite(SOLENOID_PIN, HIGH); // Unlock door
-        while (timeSince_ms(security_timer) < 30000) {
-            if (door_state == DOOR_STATES::CLOSED) {
-                if ((timeSince_ms(security_timer) > 10000) && (lock_state == LOCK_STATES::UNLOCKED) ) { // only re-lock if the lock is unlocked
-                    // Door will re-lock after 10 seconds if not opened
-                    // (i.e. unlocked but not entered)
-                    Serial.println("LOCKING DOOR2");
-                    lock_state = LOCK_STATES::LOCKED;
-                    digitalWrite(SOLENOID_PIN, LOW);
-                    break;
-                }
-//                if ((timeSince_ms(last_door_open_time) < 10000) && (lock_state == LOCK_STATES::UNLOCKED)) {
-//                    // Door will re-lock after it has been opened in the past 10 seconds
-//                    // (i.e. opened then closed)             // Now happening in ISR
-//                    Serial.println("LOCKING DOOR3");
-//                    lock_state = LOCK_STATES::LOCKED;
-//                    digitalWrite(SOLENOID_PIN, LOW);
-//                }
-            }
+        authorizedEntry();
 
-            // starts warning buzzer if door is open
-            switch (system_mode) {
-                case (SYSTEM_MODES::ARMED):
-                case (SYSTEM_MODES::AT_HOME):
-                    if (timeSince_ms(last_door_open_time) < 30000) {
-                        beep(1000, 50, 500);  // starts warning buzzer if door is open
-                    }
-                    break;
-                case (SYSTEM_MODES::DISARMED):
-                default:
-                    break;
-            }
-        }
-//        if ((authorization == AUTHORISATION_STATES::UNAUTHORISED) && (timeSince_ms(last_door_open_time) < 15*60*1000)) {
-//            alarmOn(); // alarm switching on
-//        }
+
         delay(10);
     }
 
 
     // unauthorised entry
     if (door_state == DOOR_STATES::OPEN) { // triggered by door opening without unlocking
-        switch (system_mode) {
-            case (SYSTEM_MODES::ARMED):   // when cases don't have a break, they fall through to the next case (i.e. this means OR)
-            case (SYSTEM_MODES::AT_HOME):
-                alarmOn(); // alarm switching on
-                break;
-            case (SYSTEM_MODES::DISARMED):
-            default:
-                break;
-        }
-    }
+        unauthorizedEntry(UNAUTHORISED_ENTRY_METHODS::DOOR_OPENED);
 
-    if (timeSince_ms(last_alarm_on_time) > ALARM_TIMEOUT) {
-        Serial.println("Alarm timeout");
-        alarmOff();
     }
 }
 
